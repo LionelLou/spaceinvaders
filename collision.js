d08865f7361be54151062272f2c2fec12a3d4435
Lionel Lourenço <45790701+LionelLou@users.noreply.github.com>
2021-09-01 00:46:19 +0200

    Add files via upload

---
+++ collision.js
@@ -0,0 +1,26 @@
+
+
+
+// Gestion de colision Monstre Projectile et des Hitboxs
+
+function collisionDetection( laserX, laserY, monsterXY ){
+
+    let monsterPosition = -1;
+
+    for( let i = 0 ; i < monsterXY.length ; i++ ){
+
+        let distanceX = Math.abs( laserX  - Math.abs( monsterXY[i].X ) );
+
+        let distanceY = Math.abs( laserY  - Math.abs( monsterXY[i].Y ) );
+
+
+        if ( distanceX <= monsterXY[i].size && distanceY <= monsterXY[i].size / 2  ) {
+            
+            monsterPosition = i;
+
+        }
+    }
+
+    return monsterPosition ;
+
+}
